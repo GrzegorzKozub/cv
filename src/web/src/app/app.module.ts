701b7dc0921a9a8ddc3a42c7ff5222a7f5e3f610
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2017-10-10 13:44:42 +0200

    Add cv and footer modules

--- src/web/src/app/app.module.ts
+++ src/web/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CvModule } from './cv/cv.module';
+import { FooterModule } from './footer/footer.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CvModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
