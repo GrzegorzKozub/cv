eb54d1c02d8305adf558ecad862a5687bfd057ce
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2018-05-11 19:52:40 +0200

    Resolve version on build rather than runtime

---
+++ src/api/api/Core/VersionService.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace api.Core
+{
+    public interface IVersionService
+    {
+        Task<string> Get();
+    }
+
+    internal class VersionService : IVersionService
+    {
+        public async Task<string> Get()
+        {
+            try
+            {
+                var version = await File.ReadAllTextAsync("version.txt");
+                return version.Trim();
+            }
+            catch
+            {
+                return await Task.FromResult(string.Empty);
+            }
+        }
+    }
+}
