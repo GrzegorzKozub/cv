46bd746232d266584f3fefcfa4481ff02e69713c
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2018-11-29 20:09:30 +0100

    Remove service cache tests as they were not working

--- src/web/src/app/core/cv/cv.service.spec.ts
+++ src/web/src/app/core/cv/cv.service.spec.ts
@@ -1,36 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, getTestBed, inject, TestBed } from '@angular/core/testing';
-import { BaseRequestOptions, ConnectionBackend, RequestOptions, Response, ResponseOptions } from '@angular/http';
-import { MockBackend, MockConnection } from '@angular/http/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
 
-import { environment } from '../../../environments/environment';
-import { cvFake } from './cv.fake';
 import { CvService } from './cv.service';
 
 describe('CvService', () => {
-  let connections: number;
-  let lastConnection: MockConnection;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [
-        { provide: ConnectionBackend, useClass: MockBackend },
-        { provide: RequestOptions, useClass: BaseRequestOptions },
-        CvService
-      ]
-    });
-
-    connections = 0;
-    lastConnection = undefined;
-
-    const mockBackend = getTestBed().get(ConnectionBackend) as MockBackend;
-    mockBackend.connections.subscribe((connection: MockConnection) => {
-      connections++;
-      lastConnection = connection;
-      lastConnection.mockRespond(new Response(new ResponseOptions({
-        body: cvFake
-      })));
+      providers: [CvService]
     });
   });
 
@@ -71,21 +48,6 @@ describe('CvService', () => {
   describe('getEducation', () => {
     it('should return education', async(inject([CvService], (service: CvService) => {
       service.getEducation().subscribe(education => expect(education).toBeDefined());
-    })));
-  });
-
-  describe('getCv', () => {
-    it('should fetch data only when no cache', async(inject([CvService], (service: CvService) => {
-      expect(connections).toEqual(0);
-      expect(lastConnection).toBeUndefined();
-      service.getCv().subscribe(() => {
-        expect(connections).toEqual(1);
-        service.getCv().subscribe(() => {
-          expect(connections).toEqual(1);
-          expect(lastConnection).toBeDefined();
-          expect(lastConnection.request.url).toEqual(environment.apiUrl + 'cv.json');
-        });
-      });
     })));
   });
 });
--- src/web/src/app/core/footer/footer.service.spec.ts
+++ src/web/src/app/core/footer/footer.service.spec.ts
@@ -1,18 +1,12 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, getTestBed, inject, TestBed } from '@angular/core/testing';
-import { BaseRequestOptions, ConnectionBackend, RequestOptions, Response, ResponseOptions } from '@angular/http';
-import { MockBackend, MockConnection } from '@angular/http/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subject } from 'rxjs';
 
-import { environment } from '../../../environments/environment';
-import { footerFake } from './footer.fake';
 import { FooterService } from './footer.service';
 
 describe('FooterService', () => {
   let queryParams: Subject<Params>;
-  let connections: number;
-  let lastConnection: MockConnection;
 
   beforeEach(() => {
     queryParams = new Subject<Params>();
@@ -24,22 +18,8 @@ describe('FooterService', () => {
           provide: ActivatedRoute,
           useValue: { snapshot: { queryParams: queryParams } }
         },
-        { provide: ConnectionBackend, useClass: MockBackend },
-        { provide: RequestOptions, useClass: BaseRequestOptions },
         FooterService
       ]
-    });
-
-    connections = 0;
-    lastConnection = undefined;
-
-    const mockBackend = getTestBed().get(ConnectionBackend) as MockBackend;
-    mockBackend.connections.subscribe((connection: MockConnection) => {
-      connections++;
-      lastConnection = connection;
-      lastConnection.mockRespond(new Response(new ResponseOptions({
-        body: footerFake
-      })));
     });
   });
 
@@ -69,19 +49,6 @@ describe('FooterService', () => {
   describe('getFooter', () => {
     it('should return footer', async(inject([FooterService], (service: FooterService) => {
       service.getFooter().subscribe(footer => expect(footer).toBeDefined());
-    })));
-
-    it('should fetch data only when no cache', async(inject([FooterService], (service: FooterService) => {
-      expect(connections).toEqual(0);
-      expect(lastConnection).toBeUndefined();
-      service.getFooter().subscribe(() => {
-        expect(connections).toEqual(1);
-        service.getFooter().subscribe(() => {
-          expect(connections).toEqual(1);
-          expect(lastConnection).toBeDefined();
-          expect(lastConnection.request.url).toEqual(environment.apiUrl + 'footer.json');
-        });
-      });
     })));
   });
 });
